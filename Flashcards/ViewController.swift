d6eaf3b8218d6f18d6787a34003a4de420e3b59a
Eddie Etheridge <eddie@Eddies-MacBook-Pro.local>
2018-11-24 17:26:10 -0500

    Finalized app, will go back for optionals

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var card: UIView!
+    
     
     //Array to hold our flashcards
     var flashcards = [Flashcard]()
@@ -44,7 +46,55 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true;
+        flipFlashcard()
+    }
+    
+    func flipFlashcard() {
+        UIView.transition(with: card, duration: 0.3, options: .transitionFlipFromRight, animations: {
+            self.frontLabel.isHidden = true;
+        })
+    }
+    
+    func animateCardOut() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.card.transform = CGAffineTransform.identity.translatedBy(x: -300.0, y: 0.0)
+        }, completion: { finished in
+            
+            //Update labels
+            self.updateLabels()
+            
+            //Run other animation
+            self.animateCardIn()
+        })
+    }
+    
+    func animateCardIn() {
+        //Start on the right side without animation
+        self.card.transform = CGAffineTransform.identity.translatedBy(x: 300.0, y: 0.0)
+        UIView.animate(withDuration: 0.3) {
+            self.card.transform = CGAffineTransform.identity
+        }
+    }
+    
+    func prevAnimateCardOut() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.card.transform = CGAffineTransform.identity.translatedBy(x: 300.0, y: 0.0)
+        }, completion: { finished in
+            
+            //Update labels
+            self.updateLabels()
+            
+            //Run other animation
+            self.prevAnimateCardIn()
+        })
+    }
+    
+    func prevAnimateCardIn() {
+        //Start on the left side without animation
+        self.card.transform = CGAffineTransform.identity.translatedBy(x: -300.0, y: 0.0)
+        UIView.animate(withDuration: 0.3) {
+            self.card.transform = CGAffineTransform.identity
+        }
     }
     
     func updateNextPrevButtons(){
@@ -93,11 +143,10 @@ class ViewController: UIViewController {
         //Increase current index
         currentIndex = currentIndex + 1
         
-        //Update labels
-        updateLabels()
-        
         //Update Buttons
         updateNextPrevButtons()
+        
+        animateCardOut()
     }
     
     @IBAction func didTapOnPrev(_ sender: Any) {
@@ -105,11 +154,10 @@ class ViewController: UIViewController {
         //Decrease current index
         currentIndex = currentIndex - 1
         
-        //Update labels
-        updateLabels()
-        
         //Update Buttons
         updateNextPrevButtons()
+        
+        prevAnimateCardOut()
     }
     
     func updateLabels() {
--- readme.md
+++ readme.md
@@ -1,5 +1,26 @@
 ## Flashcard App
 
+## Lab 4
+
+### App Description
+Flashcard app allowing users to create flashcards which are saved between multiple instances. Added animations for better user viewing.
+
+### App Walk-though
+
+<img src="https://media.giphy.com/media/1Xi9cpds9YCA2pf9QU/giphy.gif" width=200><br>
+
+## Required
+- [x] User should see the app icon 
+- [x] User should see a splash screen
+- [x] User should see an flip animation when revealing the answer
+- [x] User should see a slide animation when going to the next flashcard
+- [x] User should see a slide animation when going to the previous flashcard
+- [x] Push code to GitHub
+## Optional
+- [ ] User should see a nicer flaschard with rounded corners, shadows, and animating when showing the first time
+- [ ] User should see all views properly layout when using the app on a phone of different sizes
+- [ ] Correct answer in multiple choice should be random
+
 ## Lab 3
 
 ### App Description
